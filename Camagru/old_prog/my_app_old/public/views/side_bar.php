ee222677eb2a988d77ed48694ff8929189945c8c
ErnestoAvedillo <eavedillo@yahoo.es>
2025-08-05 07:00:03 +0200

    old version in not needed any more

--- Camagru/old_prog/my_app_old/public/views/side_bar.php
+++
@@ -1,8 +0,0 @@
-<aside id="sidebar">
-    <h2>Sidebar</h2>
-    <ul>
-        <li><a href="profile.php">Profile</a></li>
-        <li><a href="settings.php">Settings</a></li>
-        <li><a href="help.php">Help</a></li>
-    </ul>
-</aside>
