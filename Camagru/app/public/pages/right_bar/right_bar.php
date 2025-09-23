e05c3f0c24e189516748f62273f35eff2546e38e
ErnestoAvedillo <eavedillo@yahoo.es>
2025-09-23 16:26:12 +0200

    implement 2FA in right bar

--- Camagru/app/public/pages/right_bar/right_bar.php
+++ Camagru/app/public/pages/right_bar/right_bar.php
@@ -1,3 +1,6 @@
+<?php
+require_once __DIR__ . '/../../class_session/session.php';
+?>
 <aside id="right">
   <link rel="stylesheet" href="/pages/right_bar/right_bar.css">
   <div class="right-bar-header">
@@ -7,7 +10,21 @@
     <ul class="user-nav-right" id="userNavRight">
       <li><a href="/pages/logout/logout.php" <?php echo (basename($_SERVER['SCRIPT_NAME']) == 'logout.php') ? 'class="active"' : ''; ?>><span class="icon">🚪</span>Logout</a></li>
       <li><a href="/pages/profile/profile.php" <?php echo (basename($_SERVER['SCRIPT_NAME']) == 'profile.php') ? 'class="active"' : ''; ?>><span class="icon">👤</span>Profile</a></li>
+      <?php
+      $is2FAEnabled = SessionManager::getSessionKey('two_factor_enabled');
+      file_put_contents('/tmp/debug.log', "Current 2FA status: " . var_export($is2FAEnabled, true) . "\n", FILE_APPEND);
+      if (!$is2FAEnabled) { ?>
+        <li><a href="/pages/2FA_config/2FA_config.php" <?php echo (basename($_SERVER['SCRIPT_NAME']) == '2FA_config.php') ? 'class="active"' : ''; ?>><span class="icon">🔒</span>Enable 2FA</a></li>
+      <?php } else { ?>
+        <li><a href="/pages/2FA_config/2FA_disable.php" <?php echo (basename($_SERVER['SCRIPT_NAME']) == '2FA_enable.php') ? 'class="active"' : ''; ?>><span class="icon">🔒</span>Disable 2FA</a></li>
+      <?php } ?>
     </ul>
   </nav>
 </aside>
+<?php
+foreach ($_SESSION as $key => $value) {
+  file_put_contents('/tmp/debug.log', "Session Key: $key => Value: " . var_export($value, true) . " Get en Session:" . var_export(SessionManager::getSessionKey($key), true) . "\n", FILE_APPEND);
+}
+// Debugging line to check current 2FA status
+?>
 <script src="/pages/right_bar/hide_right_bar.js"></script>
